218fea3711c90a5abf28dac50394d74fcada34ce
Huang Qiming <76151795+ceilingFan456@users.noreply.github.com>
2021-12-17 11:46:05 +0800

    day16 complicated implementation + recursion

--- day16/main2.cpp
+++ day16/main2.cpp
@@ -27,7 +27,7 @@ int return_bits(int n) {
   return a;
 }
 
-int unpack() {
+pair<int,long long> unpack() {
   int version = return_bits(3);
   sum_ver += version;
   int type_id = return_bits(3);
@@ -35,7 +35,7 @@ int unpack() {
 
   if (type_id == 4) {
     int cur = 0;
-    long value = 0;
+    long long value = 0;
     do {
       cur = return_bits(5);
       bits_consumed += 5;
@@ -43,31 +43,80 @@ int unpack() {
       value = (value << 4) + a;
     } while(cur > 15); // 11111 - 10000 continue
 
-    cout << value << endl;
-    return bits_consumed;
+    return make_pair(bits_consumed, value);
   } 
+
+
   int length_type = return_bits(1);
   bits_consumed += 1;
+  queue<long long> values;
+
   if (length_type == 0) {
     int total_length = return_bits(15);
     bits_consumed += 15;
     int counter = 0;
     while (counter < total_length) {
-      int cur = unpack();
+      pair<int, long long> cur_pair = unpack();
+      int cur = cur_pair.first;
+      values.push(cur_pair.second);
       counter += cur;
       bits_consumed += cur;
     }
-    return bits_consumed;
   }
   else { // length_type == 1
     int total_packets = return_bits(11);
     bits_consumed += 11;
     for (int i = 0; i < total_packets; i++) {
-      int cur = unpack();
+      pair<int, long long> cur_pair = unpack();
+      int cur = cur_pair.first;
+      values.push(cur_pair.second);
       bits_consumed += cur;
     }
-    return bits_consumed;
   }
+
+  long long value = 0; 
+  if (type_id == 0) {
+    value = 0;
+    while (!values.empty()) {
+      value += values.front(); values.pop();
+    }
+  }
+  else if (type_id == 1) {
+    value = 1;
+    while (!values.empty()) {
+      value *= values.front(); values.pop();
+    }
+  }
+  else if (type_id == 2) {
+    value = LLONG_MAX;
+    while (!values.empty()) {
+      value = value > values.front() ? values.front() : value;
+      values.pop();
+    }
+  }
+  else if (type_id == 3) {
+    value = -1;
+    while (!values.empty()) {
+      value = value < values.front() ? values.front() : value;
+      values.pop();
+    }
+  }
+  else if (type_id == 5) {
+    long long a = values.front(); values.pop();
+    long long b = values.front(); values.pop();
+    value = a > b ? 1 : 0;
+  }
+  else if (type_id == 6) {
+    long long a = values.front(); values.pop();
+    long long b = values.front(); values.pop();
+    value = a < b ? 1 : 0;
+  }
+  else if (type_id == 7) {
+    long long a = values.front(); values.pop();
+    long long b = values.front(); values.pop();
+    value = a == b ? 1 : 0;
+  }
+  return make_pair(bits_consumed, value);
 }
 
 int main() {
@@ -87,7 +136,7 @@ int main() {
     src.push(b);
     p++;
   }
-  unpack();
-  cout << sum_ver << endl;
+  long long value = unpack().second;
+  cout << value << endl;
   return 0;
 }
