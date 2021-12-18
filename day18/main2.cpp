2a8497da080f83870c4de50a2aaa8fefb0f91ff1
Huang Qiming <76151795+ceilingFan456@users.noreply.github.com>
2021-12-18 17:51:26 +0800

    day18 complicated implementation + flatten a tree

--- day18/main2.cpp
+++ day18/main2.cpp
@@ -15,78 +15,52 @@ void read_number(char *p, vector<pair<int, int> > &lst) {
   }
 }
 
-int main() {
-  char line[1000];
-  fgets(line, 1000, stdin);
-  line[(int)strlen(line)-1] = 0;
+int calculate(char *p, char *q) {
   vector<pair<int, int> > pre;
   vector<pair<int, int> > cur; // <layer, value>
-  read_number(line, pre);
-  while (fgets(line, 1000, stdin)) {
-    if (line[(int)strlen(line)-1] == '\n') line[(int)strlen(line)-1] = 0;
-    read_number(line, cur); 
+  read_number(p, pre);
+  read_number(q, cur); 
+  for (int i = 0; i < pre.size(); i++) {
+    pair<int, int> a = make_pair(pre[i].first+1, pre[i].second);
+    pre[i] = a;
+  }
+  for (int i = 0; i < cur.size(); i++) {
+    pair<int, int> a = make_pair(cur[i].first+1, cur[i].second);
+    pre.push_back(a);
+  }
+  bool reduced = true;
+  while(reduced) {
+    reduced = false;
     for (int i = 0; i < pre.size(); i++) {
-      pair<int, int> a = make_pair(pre[i].first+1, pre[i].second);
-      pre[i] = a;
-    }
-    for (int i = 0; i < cur.size(); i++) {
-      pair<int, int> a = make_pair(cur[i].first+1, cur[i].second);
-      pre.push_back(a);
-    }
-    bool reduced = true;
-    while(reduced) {
-      reduced = false;
-      for (int i = 0; i < pre.size(); i++) {
-        if (pre[i].first >= 4) { // i is the left element
-          reduced = true;
-          if (i > 0) {
-            pair<int, int> front = make_pair(pre[i-1].first, pre[i-1].second+pre[i].second);
-            pre[i-1] = front;
-          }
-          if (i+1 < pre.size()-1) {
-            pair<int, int> back = make_pair(pre[i+2].first, pre[i+2].second+pre[i+1].second);
-            pre[i+2] = back;
-          }
-          pair<int, int> middle = make_pair(pre[i].first-1, 0);
-          pre.erase(pre.begin()+i);
-          pre[i] = middle;
+      if (pre[i].first >= 4) { // i is the left element
+        reduced = true;
+        if (i > 0) {
+          pair<int, int> front = make_pair(pre[i-1].first, pre[i-1].second+pre[i].second);
+          pre[i-1] = front;
         }
-      }
-      for (int i = 0; i < pre.size(); i++) {
-        if (pre[i].second > 9) {
-          reduced = true;
-          int a = pre[i].second;
-          int small = a / 2;
-          int big = a % 2 == 1 ? a / 2 + 1 : a / 2;
-          //pair<int, int> front = make_pair(pre[i].first+1, small);
-          //pair<int, int> back = make_pair(pre[i].first+1, big);
-          int l = pre[i].first;
-          pre.erase(pre.begin()+i);
-          pre.insert(pre.begin()+i, make_pair(l+1, big));
-          pre.insert(pre.begin()+i, make_pair(l+1, small));
-          break;
+        if (i+1 < pre.size()-1) {
+          pair<int, int> back = make_pair(pre[i+2].first, pre[i+2].second+pre[i+1].second);
+          pre[i+2] = back;
         }
+        pair<int, int> middle = make_pair(pre[i].first-1, 0);
+        pre.erase(pre.begin()+i);
+        pre[i] = middle;
       }
-      /*
-  for (int i = 0; i < pre.size(); i++) {
-    printf("(%d, %d), ", pre[i].first, pre[i].second);
-  }
-  printf("\n");
-  */
     }
-    cur.clear();
-    /*
-  for (int i = 0; i < pre.size(); i++) {
-    printf("(%d, %d), ", pre[i].first, pre[i].second);
+    for (int i = 0; i < pre.size(); i++) {
+      if (pre[i].second > 9) {
+        reduced = true;
+        int a = pre[i].second;
+        int small = a / 2;
+        int big = a % 2 == 1 ? a / 2 + 1 : a / 2;
+        int l = pre[i].first;
+        pre.erase(pre.begin()+i);
+        pre.insert(pre.begin()+i, make_pair(l+1, big));
+        pre.insert(pre.begin()+i, make_pair(l+1, small));
+        break;
+      }
+    }
   }
-  printf("\n");
-  */
-  }
-
-  for (int i = 0; i < pre.size(); i++) {
-    printf("(%d, %d), ", pre[i].first, pre[i].second);
-  }
-  printf("\n");
   while (pre.size() > 1) {
     for (int i = 0; i < pre.size()-1; i++) {
       if (pre[i].first == pre[i+1].first) {
@@ -99,6 +73,32 @@ int main() {
       }
     }
   }
-  cout << pre[0].second << endl;
+  return pre[0].second;
+}
+
+int main() {
+  char line[1000];
+  char lst[200][200];
+  int n = 0;
+  while (fgets(line, 1000, stdin)) {
+    if (line[(int)strlen(line)-1] == '\n') line[(int)strlen(line)-1] = 0;
+    char *p = line;
+    int m = 0;
+    while (*p != 0) {
+      lst[n][m] = *p;
+      p++;
+      m++;
+    }
+    n++;
+  }
+  int biggest = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i == j) continue;
+      int cur = calculate(lst[i],lst[j]);
+      if (cur > biggest) biggest = cur;
+    }
+  }
+  cout << biggest << endl;
   return 0;
 }
